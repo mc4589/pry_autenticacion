860df525520425ca9599d8366ab80fa4e94020db
Ser <sicondo@espe.edu.ec>
2025-11-24 15:12:27 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,14 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AuthController;//
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-// rutas públicas
-Route::post('/register', [UserController::class, 'store']);
-Route::post('/login',    [AuthController::class, 'login']);        
+// rutas públicas, no requieren que el usuario este autenticado
+Route::post('/register', [UserController::class, 'store']);//creamos un nuevo usuario, con una ruta de tipo POST en la URl/registrer, cuando acceda ejecutara el metod store del UserController
+Route::post('/login',    [AuthController::class, 'login']);// Ejecuta el método login del AuthController. Se usa para que un usuario inicie sesión y obtenga un token de acceso.   
 
-// rutas protegidas con Sanctum
+// rutas protegidas con Sanctum, por tanto requieren autenticacion
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/logout', [UserController::class, 'logout']);
