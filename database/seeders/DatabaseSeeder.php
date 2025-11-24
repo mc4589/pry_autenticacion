d0eb0f0def327e1e27b83aadfdda7a1419735c09
Jessy_Mariño <sjmarino1@espe.edu.ec>
2025-11-24 17:39:42 -0500

    Update DatabaseSeeder to create specific user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,25 +1,18 @@
 <?php
-
 namespace Database\Seeders;
-
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
-{
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
     {
-        // User::factory(10)->create();
-
+        public function run(): void
+        {
+             // 1. Crea un usuario específico 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'nombre' => 'Camilo López',
+            'email' => 'camilo@example.com',
+            'perfil' => 'administrador',
         ]);
+        }
     }
-}
+    
+            
