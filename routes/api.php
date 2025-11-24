f781f6fc8de9f023d620209f232eb66f6d48ddd3
Ser <sicondo@espe.edu.ec>
2025-11-24 14:39:01 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// rutas públicas
+Route::post('/register', [UserController::class, 'store']);
+Route::post('/login',    [AuthController::class, 'login']);        
+
+// rutas protegidas con Sanctum
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::get('/user',    [UserController::class, 'show']);
+
+    // validate-token 
+    Route::get('/validate-token', [AuthController::class, 'validateToken']);
+});
