21a84f7a209f15cdd7200e150bfc41d644ecade3
Eduardo Cantuña <118578087+EduardoCantunaCela@users.noreply.github.com>
2025-11-24 19:09:36 -0500

    users table migration
    Create the users table with necessary fields.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,6 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');                            
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+
 
 
 };
